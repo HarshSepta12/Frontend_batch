a3869a6b8ab19617ec059ac7a1397f4c41d38660
harshservi48@gmail.com <harshservi48@gmail.com>
2024-04-17 16:58:57 +0530

    Day28 Day29  Day29 Day30 Javascript

---
+++ Day30 javascript intro/index.js
@@ -0,0 +1,15 @@
+// console.log("Harsh Septa");
+console.log("We are learning javascript right now");
+let name0 = "Ram";
+var name1 = "Shree";
+const name2 = "Sita Ram";
+console.log(name0);
+console.log(name1);
+console.log(name2);
+// const name2 = "Ram Ram";
+let number = 12;
+if(number == 12){
+    console.log("Yes it is equal to 12");
+}else{
+    console.log("It's not qual to 12");
+}
