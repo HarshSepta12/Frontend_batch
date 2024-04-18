80e5997ef8dd0907b21a0afd55cc49a30b859925
harshservi48@gmail.com <harshservi48@gmail.com>
2024-04-18 17:00:19 +0530

    If Else backtick

---
+++ Day31 JS/app.js
@@ -0,0 +1,36 @@
+// console.log("Harsh");
+const hero1 = "Superman";
+const hero2 = "Spiderman";
+
+// "Superman" is belong from "DC" and "Spiderman" is belong from "Marvel"
+// backtick
+// console.log(` '${hero1}' is belong from DC and '${hero2}' is belong from Marvel `);
+
+// operator 
+// const num1 = 200;
+// const num2 = 100;
+// console.log(`Sum of ${num1} and &{num2} = ${num1+num2}`);
+// console.log(`Substraction of ${num1} and &{num2} = ${num1-num2}`);
+// console.log(`Multiplication value of ${num1} and &{num2} = ${num1*num2}`);
+// console.log(`dividation of ${num1} and &{num2} = ${num1/num2}`);
+// console.log(`Modulose of ${num1} and &{num2} = ${num1%num2}`);
+
+
+// typeof function 
+// const num = 100;
+// const num1 = '100';
+// const num2 = 100.44;
+// console.log(typeof(num));
+// console.log(typeof(num1));
+// console.log(typeof(num2));
+
+
+// if else 
+const num = '10';
+// == only check the variable value
+//=== check its data type and value
+if(num === 10){
+    console.log("This is a number");
+}else{
+    console.log('This is string');
+}
---
+++ Day31 JS/if_else.js
@@ -0,0 +1,76 @@
+// const AdharCard = true;
+// const  panCard = true;
+// const  pan = false;
+// let age =18;
+
+// if(panCard && AdharCard){
+//     console.log("You can open Acount is our bank");
+//     // nested if else 
+//     if(age >= 18){
+//         console.log("You are eligible for car insurence");
+//     }else{
+//         console.log("Your age should be greater 18 to clam car insurence");
+//     }
+// }else{
+//     console.log("Your can not open your acount in ourbank");
+// }
+
+
+// if(panCard || AdharCard){
+//     console.log("You can open Acount is our bank");
+// }else{
+//     console.log("Your can not open your acount in ourbank");
+// }
+// if(pan && AdharCard){
+//     console.log("You can open Acount is our bank");
+// }else{
+//     console.log("Your can not open your acount in ourbank");
+// }
+
+
+// const physic = 80;
+// const chemistry = 83;
+// const Cs = 81;
+// const maths = 85;
+// const english = 85;
+// const level1 = true;
+// const level2 = true;
+// const level3 = true;
+
+// const avg = (physic + chemistry + Cs + maths + english)/5;
+// console.log("Your marks is: "+ avg);
+// if(avg >= 80){
+//     // console.log("You are eligible for olampic");
+//     if(level1 && level2 && level3){
+//         console.log("Your are eligible for olampic and you won 10000")
+//     }
+// }else{
+//     console.log("You are not eligible for olampic");
+// }
+
+
+let Day = 4;
+if(Day == 1){
+    console.log("Today is Monday");
+}
+else if(Day == 2){
+    console.log("Today is tuesday");
+}
+else if(Day == 3){
+    console.log("Today is wednesday");
+}
+else if(Day == 4){
+    console.log("Today is Thursday");
+}
+else if(Day == 5){
+    console.log("Today is Friday");
+}
+else if(Day == 6){
+    console.log("Today is Saturday");
+}
+else if(Day == 7){
+    console.log("Today is Sunday");
+}
+else{
+    console.log('Bich ka din he')
+}
