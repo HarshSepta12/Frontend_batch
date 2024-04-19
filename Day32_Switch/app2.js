cf90463b44ffce800edd14d4d426213cf228d6f9
harshservi48@gmail.com <harshservi48@gmail.com>
2024-04-19 17:24:07 +0530

    Switch Currency converter

---
+++ Day32_Switch/app2.js
@@ -0,0 +1,21 @@
+const operator = 3;
+const num1 = 12;
+const num2 =12;
+
+switch(operator){
+    case 1:
+        console.log(num1+num2);
+        break;
+    case 2:
+        console.log(num1-num2);
+        break;
+    case 3:
+        console.log(num1*num2);
+        break;
+    case 4:
+        console.log(num1/num2);
+        break;
+    default:
+        console.log("Wrong input");
+    
+}
