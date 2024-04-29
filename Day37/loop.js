1706960ef7993e5463e8ea908df3a006f43b8217
harshservi48@gmail.com <harshservi48@gmail.com>
2024-04-29 17:27:07 +0530

    Normal Practice

---
+++ Day37/loop.js
@@ -0,0 +1,26 @@
+// let list = [1, 4, 5, 6, 7,8];
+// let high = list[0];
+// let sh;
+// for(let i=1; i<list.length; i++){
+//     if(high<list[i]){
+//         sh = high;
+//         high = list[i]
+//     }
+//   else if(sh<list[i]){
+//     sh = list[i];
+//   }
+// }
+// console.log(sh);
+// console.log(high);
+
+
+let arr = [12,24,36,48,60,72,84,96,108];
+let  result =arr.length%2!=0 ? (arr.length+1)/2 : (arr.length)/2;
+console.log(result)
+
+
+
+   
+
+
+
