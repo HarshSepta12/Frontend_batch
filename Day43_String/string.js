e111d72463acaa15470944b21cd939a8538e6b46
harshservi48@gmail.com <harshservi48@gmail.com>
2024-05-07 17:21:03 +0530

    String Intro

---
+++ Day43_String/string.js
@@ -0,0 +1,9 @@
+const str = "Superman";
+// to change to string to upper case
+console.log(str.toUpperCase());
+
+// to change to string to lower case
+console.log(str.toLowerCase());
+
+const str1 = "  Superman Sharma ji  ";
+console.log(str1.trim());
