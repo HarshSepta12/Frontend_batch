75f359a37c07a65c6ed6365925847d216cc1c717
harshservi48@gmail.com <harshservi48@gmail.com>
2024-05-28 17:27:13 +0530

    API

---
+++ Day54_javaScript_Async_Await/index.js
@@ -0,0 +1,9 @@
+// fetch  Data From API
+const fetchDataFromAPI = async ()=>{
+    const api = await fetch('https://jsonplaceholder.typicode.com/photos');
+    const data = await api.json();
+    console.log(data);
+}
+
+fetchDataFromAPI()
+console.log("Hello Console");
