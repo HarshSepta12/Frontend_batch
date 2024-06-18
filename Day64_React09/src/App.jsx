d17402c11576550767b43997bf49ea7393a9f56d
harshservi48@gmail.com <harshservi48@gmail.com>
2024-06-18 16:29:13 +0530

    Day64 useEffect

---
+++ Day64_React09/src/App.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react'
+
+const App = () => {
+  const [counter,setCounter] = useState(0)
+  useEffect(() =>{
+    document.title = counter
+    console.log("UseEffect is running...!");
+  },[counter]) // Dependency Array
+
+
+  return (
+    <>
+      <h1>{counter}</h1>
+      <button onClick={()=> setCounter(counter + 1)}>Increase</button>
+      <button onClick={()=> setCounter(counter - 1)}>Decrease</button>
+    </>
+  )
+}
+
+export default App
